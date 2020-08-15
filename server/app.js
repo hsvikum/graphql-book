a304152488b407e672eeb2fa8cbc899aeb3ced7f
Shalitha Vikum <hsvikum@gmail.com>
2020-08-15 22:22:32 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
 
 mongoose.connect('<Server URL String Here>', {
     useNewUrlParser: true,
